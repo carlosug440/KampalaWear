77132966267e04188fd87445183ba447fbe432f4
CarlosDeUganda <carlosdeuganda@gmail.com>
2026-04-05 04:08:08 +0300

    Update and rename products.css to products.js

---
+++ products.js
@@ -0,0 +1,6 @@
+
+const products = [
+    { id: 1, name: "Red T-shirt", price: 1500, stock: 10 },
+    { id: 2, name: "Blue Jeans", price: 2500, stock: 5 },
+    { id: 3, name: "Sneakers", price: 3500, stock: 7 }
+];
