7098a4890a30fb8a057b1d69218730e37149f308
CarlosDeUganda <carlosdeuganda@gmail.com>
2026-04-05 04:27:55 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,5 @@
-
 const products = [
-    { id: 1, name: "Red T-shirt", price: 1500, stock: 10 },
-    { id: 2, name: "Blue Jeans", price: 2500, stock: 5 },
-    { id: 3, name: "Sneakers", price: 3500, stock: 7 }
+    { id: 1, name: "Red T-shirt", price: 20, stock: 10, img: "https://cdn-images.farfetch-contents.com/35/00/77/86/35007786_66202420_600.jpg" },
+    { id: 2, name: "Blue Jeans", price: 35, stock: 5, img: "https://cdn-images.farfetch-contents.com/28/60/11/83/28601183_58252387_600.jpg" },
+    { id: 3, name: "Sneakers", price: 50, stock: 7, img: "https://cdn-images.farfetch-contents.com/23/53/42/98/23534298_53693492_600.jpg" }
 ];
